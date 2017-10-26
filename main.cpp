cf23c862d138bb0e5f9c2f5fb7cf971cf8c3899e
Souyoung Jin test <souyoung@cs.umass.edu>
2017-10-25 21:58:58 -0400

    readme_gitignore

--- main.cpp
+++ main.cpp
@@ -3,12 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <vector>
-#include <list>
 #include "Rank1Count.h"
-#include <string>
-#include <algorithm>
-#include <iterator>
 using namespace std;
 
 
